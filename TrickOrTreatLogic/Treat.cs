6806e274d2f4aed5baeafe71c6dc1df7013ca943
Jaime E Falla <65088216+Jaimedfalla@users.noreply.github.com>
2022-11-02 16:02:08 -0500

    Fix logic

--- TrickOrTreatLogic/Treat.cs
+++ TrickOrTreatLogic/Treat.cs
@@ -4,15 +4,14 @@
     {
         public Treat()
         {
-            //_tricks = new string[] { "\U0001f370", "\U0001f36c", "\U0001f361", "\U0001f36a", "\U0001f36d", "\U0001f36b", "\U0001f366", "\U0001f369" };
             _tricks = new string[] {"🍰","🍬","🍡","🍪","🍭","🍫","🧁","🍩"};
         }
 
         public string Process(IEnumerable<Person> people)
         {
             int times = people.Sum(p => p.Name.Length);
-            times += people.Sum(p => p.Age > 9? 3: p.Age/3);
-            times += people.Sum(p => (p.Height <=150? p.Height:150)/25);
+            times += people.Sum(p => p.Age <= 10? 3: 0);
+            times += people.Sum(p => (p.Height <=150? p.Height:0)/25);
 
             return Print(times);
         }
